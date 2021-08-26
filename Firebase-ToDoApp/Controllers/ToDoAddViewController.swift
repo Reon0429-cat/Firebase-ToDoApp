b9b32b274409b8c724522d6bea49ea24abdd7b44
0429oonishi <oonishireon110@gmail.com>
2021-08-27 00:22:44 +0900

    ToDoAdd実装

--- Firebase-ToDoApp/Controllers/ToDoAddViewController.swift
+++ Firebase-ToDoApp/Controllers/ToDoAddViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class ToDoAddViewController: UIViewController {
+final class ToDoAddViewController: UIViewController {
 
+    @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var detailTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTextView()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func addButtonDidTapped(_ sender: Any) {
+        
     }
-    */
+    
+}
 
+// MARK: - setup
+private extension ToDoAddViewController {
+    
+    func setupTextView() {
+        detailTextView.layer.borderWidth = 1.0
+        detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        detailTextView.layer.cornerRadius = 5.0
+        detailTextView.layer.masksToBounds = true
+    }
+    
 }
