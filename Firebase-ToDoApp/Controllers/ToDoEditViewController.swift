aa15880d99b62a3b23b75aba40619920e3ff03a1
0429oonishi <oonishireon110@gmail.com>
2021-08-27 00:22:52 +0900

    ToDoEdit作成

--- Firebase-ToDoApp/Controllers/ToDoEditViewController.swift
+++ Firebase-ToDoApp/Controllers/ToDoEditViewController.swift
@@ -7,23 +7,40 @@
 
 import UIKit
 
-class ToDoEditViewController: UIViewController {
-
+final class ToDoEditViewController: UIViewController {
+    
+    @IBOutlet private weak var titleTextField: UITextField!
+    @IBOutlet private weak var detailTextView: UITextView!
+    @IBOutlet private weak var doneButton: UIButton!
+    @IBOutlet private weak var isDoneLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTextView()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func editButtonDidTapped(_ sender: Any) {
+        
     }
-    */
+    @IBAction private func doneButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func deleteButtonDidTapped(_ sender: Any) {
+    }
+    
+}
 
+// MARK: - setup
+private extension ToDoEditViewController {
+    
+    func setupTextView() {
+        detailTextView.layer.borderWidth = 1.0
+        detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        detailTextView.layer.cornerRadius = 5.0
+        detailTextView.layer.masksToBounds = true
+    }
+    
 }
