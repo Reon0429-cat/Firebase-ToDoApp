24ea3f00ba3182a437adcc1ccba4eb295e14c3ca
0429oonishi <oonishireon110@gmail.com>
2021-08-27 16:44:17 +0900

    テーブルに反映

--- Firebase-ToDoApp/Controllers/ToDoListViewController.swift
+++ Firebase-ToDoApp/Controllers/ToDoListViewController.swift
@@ -13,8 +13,16 @@ final class ToDoListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var userNameLabel: UILabel!
     
+    private var ids = [String]()
+    private var titles = [String]()
+    private var details = [String]()
+    private var isDones = [Bool]()
+    private var isDone = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView()
         
     }
     
@@ -22,15 +30,8 @@ final class ToDoListViewController: UIViewController {
         super.viewWillAppear(animated)
         
         if let user = Auth.auth().currentUser {
-            Firestore.firestore().collection("users").document(user.uid).getDocument { snapshot, error in
-                if let snapshot = snapshot {
-                    if let data = snapshot.data() {
-                        self.userNameLabel.text = data["name"] as? String
-                    }
-                } else if let error = error {
-                    print("ユーザー名取得失敗: " + error.localizedDescription)
-                }
-            }
+            fetchUserName(user: user)
+            fetchWithSearchAndSort(user: user)
         }
         
     }
@@ -54,7 +55,77 @@ final class ToDoListViewController: UIViewController {
         }
     }
     
-    @IBAction private func segmentedControlDidChanged(_ sender: Any) {
+    @IBAction private func segmentedControlDidChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                isDone = false
+                getToDoDataFromFirestore()
+            case 1:
+                isDone = true
+                getToDoDataFromFirestore()
+            default:
+                isDone = false
+                getToDoDataFromFirestore()
+        }
+    }
+    
+    private func getToDoDataFromFirestore() {
+        if let user = Auth.auth().currentUser {
+            let storeRef = Firestore.firestore().collection("users/\(user.uid)/todos")
+            storeRef.whereField("isDone", isEqualTo: isDone).order(by: "createdAt").getDocuments { querySnapshot, error in
+                if let querySnapshot = querySnapshot {
+                    self.removeAll()
+                    for doc in querySnapshot.documents {
+                        let data = doc.data()
+                        self.ids.append(doc.documentID)
+                        self.titles.append(data["title"] as! String)
+                        self.details.append(data["detail"] as! String)
+                        self.isDones.append(data["isDone"] as! Bool)
+                    }
+                    self.tableView.reloadData()
+                } else if error != nil {
+                    print("ToDo情報取得失敗: ", error!.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func fetchWithSearchAndSort(user: User) {
+        let storeRef = Firestore.firestore().collection("users/\(user.uid)/todos")
+        storeRef.whereField("isDone", isEqualTo: isDone).order(by: "createdAt").addSnapshotListener { querySnapshot, error in
+            if let querySnapshot = querySnapshot {
+                self.removeAll()
+                for doc in querySnapshot.documents {
+                    let data = doc.data()
+                    self.ids.append(doc.documentID)
+                    self.titles.append(data["title"] as! String)
+                    self.details.append(data["detail"] as! String)
+                    self.isDones.append(data["isDone"] as! Bool)
+                }
+                self.tableView.reloadData()
+            } else if error != nil {
+                print("ToDo情報取得失敗: ", error!.localizedDescription)
+            }
+        }
+    }
+    
+    private func removeAll() {
+        self.ids.removeAll()
+        self.titles.removeAll()
+        self.details.removeAll()
+        self.isDones.removeAll()
+    }
+        
+    private func fetchUserName(user: User) {
+        Firestore.firestore().collection("users").document(user.uid).getDocument { snapshot, error in
+            if let snapshot = snapshot {
+                if let data = snapshot.data() {
+                    self.userNameLabel.text = data["name"] as? String
+                }
+            } else if let error = error {
+                print("ユーザー名取得失敗: " + error.localizedDescription)
+            }
+        }
     }
     
     private func presentToDoAddVC() {
@@ -62,6 +133,7 @@ final class ToDoListViewController: UIViewController {
         let vc = storyboard.instantiateViewController(
             identifier: "ToDoAddViewController"
         ) as! ToDoAddViewController
+        vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
     }
     
@@ -74,3 +146,42 @@ final class ToDoListViewController: UIViewController {
     }
     
 }
+
+// MARK: - UITableViewDelegate
+extension ToDoListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+extension ToDoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row]
+        return cell
+    }
+    
+}
+
+// MARK: - setup
+extension ToDoListViewController {
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
